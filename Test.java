9a7e5dc5c164dd678d614b97ae24f8b00b350ac7
Bea <bea_riego93@hotmail.com>
2015-02-09 09:16:42 +0100

    codificado metodo printProductDetails

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test
+{
+    public void test1()
+    {
+        StockManager StockManager = new StockManager();
+        
+        Product products1 = new Product(1,"a");
+        Product products2 = new Product(2,"b");
+        Product products3 = new Product(3,"c");
+        Product products4 = new Product(4,"d");
+        Product products5 = new Product(5,"e");
+        Product products6 = new Product(6,"f");
+        Product products7 = new Product(7,"g");
+        
+        products.addProduct("products");
+        products.addProduct("products");
+        products.addProduct("products");
+        
+        products.printProductDetails();
+    } 
+}
